cc890fa24bcf961fe31d304e1b22fc2102abaf9e
Joaquin <axxdderr@gmail.com>
2025-04-16 01:33:15 -0500

    module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './modules/products/controllers/products.controller';
-import { CategoriesController } from './modules/categories/controllers/categories.controller';
-import { UsersController } from './modules/users/controllers/users.controller';
-import { OrdersController } from './modules/orders/controllers/orders.controller';
-import { CustomersController } from './modules/customers/controllers/customers.controller';
-import { BrandsController } from './modules/brands/controllers/brands.controller';
-import { OrdersService } from './modules/orders/services/orders.service'; 
-import { ProductsService } from './modules/products/services/products.service';
-import { BrandsService } from './modules/brands/services/brands.service';
-import { CategoriesService } from './modules/categories/services/categories.service'; 
-import { CustomersService } from './modules/customers/services/customers.service';
-import { UsersService } from './modules/users/services/users.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { CategoriesModule } from './modules/categories/categories.module';
@@ -22,7 +10,7 @@ import { CustomersModule } from './modules/customers/customers.module';
 
 @Module({
   imports: [UsersModule, ProductsModule, CategoriesModule, BrandsModule, OrdersModule, CustomersModule],
-  controllers: [AppController, ProductsController, CategoriesController, UsersController, OrdersController, CustomersController, BrandsController],
-  providers: [AppService, ProductsService, OrdersService, BrandsService, CategoriesService, CustomersService, UsersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CustomersController } from './controllers/customers.controller';
+import { CustomersService } from './services/customers.service';
 
-@Module({})
+@Module({
+    controllers: [CustomersController],
+    providers: [CustomersService]
+})
 export class CustomersModule {}
